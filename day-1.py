20805f001602a111fe1987ece408e6838a29b100
DESKTOP-P7TDC68\Sujin <resqpa79@gmail.com>
2022-01-16 16:33:04 +0900

    first commit

---
+++ day-1.py
@@ -0,0 +1,31 @@
+# Fibonacci series:
+# the sum of two elements defined the next
+print('----------------------------')
+print('-         Fibonacci        -')
+print('----------------------------')
+
+
+def fib(n):         # write Fibonacci series up to n
+    """Print a Fibonacci series up to n."""
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+
+def fib2(n):    # return Fibonacci series up to n
+    """Return a list containing the Fibonacci series up to n."""
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)    # see below
+        a, b = b, a+b
+    return result
+
+print('Calling fib function.')
+# Now call the function we just defined:
+fib(2000)
+
+print('Calling fib2 function.')
+f100 = fib2(100)     # call it
+print(f100)                # write the result
